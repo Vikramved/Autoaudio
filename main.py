0cffce3599de0ed8a7797ec112b10cf3f22a6a8b
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-17 13:02:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from ffprobe import FFProbe
+from concurrent.futures import ThreadPoolExecutor
+from pyrogram.errors import MessageNotModified
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 api_id = "15428219"
 api_hash = "0042e5b26181a1e95ca40a7f7c51eaa7"
@@ -9,18 +15,25 @@ bot_token = "5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc"
 # Initialize the Pyrogram client
 app = Client("telegram_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-
-
 # Filter to handle incoming messages with media
 @Client.on_message(filters.media)
 def handle_media(bot: Client, message: Message):
     # Get the media file ID
     file_id = message.media.document.file_id
 
-    # Download the media file
-    file_path = bot.download_media(file_id)
+    # Use a thread pool executor for parallel processing
+    with ThreadPoolExecutor() as executor:
+        # Download the media file
+        file_path = bot.download_media(file_id)
 
-    # Use FFprobe to analyze the media file
+        # Submit the analysis task to the executor
+        future = executor.submit(analyze_media, bot, message.chat.id, file_path)
+
+        # Add a callback to send the file with the caption when analysis is complete
+        future.add_done_callback(lambda f: send_media_with_caption(bot, message.chat.id, file_id, f.result()))
+
+# Function to analyze the media file and extract the language information
+def analyze_media(bot: Client, chat_id: int, file_path: str):
     probe = FFProbe(file_path)
     audio_streams = probe.audio_streams
 
@@ -31,6 +44,9 @@ def handle_media(bot: Client, message: Message):
         if language:
             languages.add(language)
 
+    # Log the detected languages
+    logging.info(f"Bot detected languages: {languages}")
+
     # Build the caption based on the extracted audio languages
     if len(languages) > 1:
         caption = "Audio: " + "+".join(languages)
@@ -39,13 +55,23 @@ def handle_media(bot: Client, message: Message):
     else:
         caption = "No audio language information found."
 
+    return (file_path, caption)
+
+# Function to send the media file with the caption
+def send_media_with_caption(bot: Client, chat_id: int, file_id: str, result):
+    file_path, caption = result
+
+    # Log the media file detection
+    logging.info(f"Bot detected media file: {file_path}")
+
     # Send the media file with the caption as its caption
-    bot.send_document(chat_id=message.chat.id, document=file_path, caption=caption)
+    try:
+        bot.send_document(chat_id=chat_id, document=file_path, caption=caption)
+    except MessageNotModified:
+        pass
 
     # Delete the downloaded file
     bot.remove(file_path)
 
-
 # Run the bot
-print("start")
 app.run()
