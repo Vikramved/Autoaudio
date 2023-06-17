bb79886a551614f31b4226c040b3c5741e9ef7cc
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-18 01:02:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ app = Client("my_bot",
 def detect_language(client, message):
     if message.document and message.document.mime_type == "video/x-matroska":
         file_path = message.document.file_name
-    os.system(f"mkvmerge -i {file_path} -o temp.mkv")
-    os.system("ffmpeg -i temp.mkv -vn -acodec copy temp.aac")
-    languages = os.system("ffmpeg -i temp.aac -map 0:a -c:a copy -f segment -segment_time 3 - | ffprobe -v quiet -show_entries stream=tags=language -of default=nw=1:nk=1")
-    caption = f"Audio : {languages}"
-    message.reply_document(file_path, caption=caption)
-    os.remove("temp.mkv")
-    os.remove("temp.aac")
+        os.system(f"mkvmerge -i {file_path} -o temp.mkv")
+        os.system("ffmpeg -i temp.mkv -vn -acodec copy temp.aac")
+        languages = os.system("ffmpeg -i temp.aac -map 0:a -c:a copy -f segment -segment_time 3 - | ffprobe -v quiet -show_entries stream=tags=language -of default=nw=1:nk=1")
+        caption = f"Audio : {languages}"
+        message.reply_document(file_path, caption=caption)
+        os.remove("temp.mkv")
+        os.remove("temp.aac")
 
 print("startedddd")
 app.run() 
