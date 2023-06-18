a74e5f4d6c89f24348e83990dc8cffa583ab2e83
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-18 19:50:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = Client("my_bot",
             api_id="15428219", 
             api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", 
             bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc"
+            )
 
 @app.on_message(filters.document & filters.video)
 def detect_language(client, message):
