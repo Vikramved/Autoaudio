cb9e49b147777b587ff8ebae9c43e07a3465eed6
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-18 19:46:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from pyrogram import Client, filters
 app = Client("my_bot", 
             api_id="15428219", 
             api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", 
-            bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc")
+            bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc"
 
-@app.on_message() 
+@app.on_message(filters.document & filters.video)
 def detect_language(client, message):
-    if message.document and message.document.mime_type == "video/x-matroska":
-        file_path = message.document.file_name
-        os.system(f"mkvmerge -i {file_path} -o temp.mkv")
-        os.system("ffmpeg -i temp.mkv -vn -acodec copy temp.aac")
-        languages = os.system("ffmpeg -i temp.aac -map 0:a -c:a copy -f segment -segment_time 3 - | ffprobe -v quiet -show_entries stream=tags=language -of default=nw=1:nk=1")
-        caption = f"Audio : {languages}"
-        message.reply_document(file_path, caption=caption)
-        os.remove("temp.mkv")
-        os.remove("temp.aac")
+    if message.document.file_name.endswith(".mkv"):
+        caption = ""
+        for track in message.document.thumbs[0].video_info.audio_tracks:
+            if track.language:
+                if caption:
+                    caption += "+"
+                caption += track.language
+        message.reply_text(f"Audio: {caption}")
 
-print("startedddd")
+print("shahiddd")
 app.run() 
+            
