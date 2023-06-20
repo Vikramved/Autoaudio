239726a51e31bda23469b2500d8b77bdf0c3c2fd
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-21 00:25:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def handle_message(client, message):
         report_date = f"{datetime.datetime.now().strftime('%d-%m-%Y')}"
         report_day = f"{datetime.datetime.now().strftime('%A')}"
         track_id = f"#MB{random.randint(1, 1000000)}"
-        report_top = "✅ Rᴇᴘᴏʀᴛ Sᴇɴᴅ Tᴏ Dᴀsʜʙᴏᴀʀᴅ ✅"
+        report_top = "✅ Rᴇᴘᴏʀᴛ sᴇɴᴅ ᴛᴏ ᴀᴅᴍɪɴ ✅"
         report = {
             "report_top": report_top,
             "reporter": message.from_user.first_name,
@@ -29,7 +29,7 @@ async def handle_message(client, message):
         db.reports.insert_one(report)
         now_in_india = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=5, minutes=30)))
         report_time_in_india = f"{now_in_india.strftime('%I:%M:%S %p')}"
-        await message.reply(f"{report['report_top']}\n\n👤 Rᴇᴘᴏʀᴛᴇʀ: {report['reporter']}\n🆔 Rᴇᴘᴏʀᴛᴇʀ ɪᴅ: {report['reporter_id']}\n📜 Tʀᴀᴄᴋ ɪᴅ: {report['track_id']}\n💬 Rᴇᴘᴏᴛʀ ᴛᴇxᴛ : {report['report_text']}\n⌚ Rᴇᴘᴏʀᴛ ᴛɪᴍᴇ: {report_time_in_india}\n🗓️ Rᴇᴘᴏʀᴛ ᴅᴀᴛᴇ: {report['report_date']}\n⛅ Rᴇᴘᴏʀᴛ ᴅᴀʏ: {report['report_day']}")
+        await message.reply(f"{report['report_top']}\n\n👤 Rᴇᴘᴏʀᴛᴇʀ: {report['reporter']}\n🆔 Rᴇᴘᴏʀᴛᴇʀ ɪᴅ: {report['reporter_id']}\n📜 Tʀᴀᴄᴋ ɪᴅ: {report['track_id']}\n\n💬 Rᴇᴘᴏᴛʀ ᴛᴇxᴛ : {report['report_text']}\n\n⌚ Rᴇᴘᴏʀᴛ ᴛɪᴍᴇ: {report_time_in_india}\n🗓️ Rᴇᴘᴏʀᴛ ᴅᴀᴛᴇ: {report['report_date']}\n⛅ Rᴇᴘᴏʀᴛ ᴅᴀʏ: {report['report_day']}")
         channel_id = -1001904370879
         await client.send_message(channel_id, f"Reporter: {report['reporter']}\nReporter ID: {report['reporter_id']}\nTrack ID: {report['track_id']}\nReport Text: {report['report_text']}\nReport Time: {report_time_in_india}\nReport Date: {report['report_date']}\nReport Day: {report['report_day']}")
 
