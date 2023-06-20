9c32b9d77598d47f6fc4d9ceec3cd85f8e7ff7c9
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-20 18:29:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
 from pyrogram import Client, filters
+import time
 
 app = Client("my_bot", 
             api_id="15428219", 
@@ -7,17 +7,34 @@ app = Client("my_bot",
             bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc"
             )
 
-@app.on_message(filters.document & filters.video)
-def detect_language(client, message):
-    if message.document.file_name.endswith(".mkv"):
-        caption = ""
-        for track in message.document.thumbs[0].video_info.audio_tracks:
-            if track.language:
-                if caption:
-                    caption += "+"
-                caption += track.language
-        message.reply_text(f"Audio: {caption}")
+# Define the filter for messages containing "@admin"
+@admin_filter = filters.create(lambda _, __, message: "@admin" in message.text.lower())
 
-print("shahiddd")
-app.run() 
-            
+# Handle the messages that match the filter
+@app.on_message(admin_filter)
+def handle_admin_message(client, message):
+    # Get the sender's username and Telegram user id
+    sender_username = message.from_user.username
+    sender_id = message.from_user.id
+
+    # Get the report text and current time
+    report_text = message.text.replace("@admin", "").strip()
+    report_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    # Build the reply message
+    reply_message = (
+        "Report sent successfully\n"
+        f"Reporter: {sender_username}\n"
+        f"Reporter Id: {sender_id}\n"
+        f"Track id: #MB{sender_id % 10}{sender_id % 10 + 1}\n"
+        f"Report Text: {report_text}\n"
+        f"Report Time: {report_time}"
+    )
+
+    # Reply to the message
+    message.reply_text(reply_message)
+
+
+# Start the bot
+print("Started")
+app.run()
