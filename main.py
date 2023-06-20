5879768e69d3bc43744c6af3a5993fc8baa82b06
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-20 22:09:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyrogram
 import datetime
+import random
 
 bot = pyrogram.Client("my_bot", api_id="15428219", api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc")
 
@@ -8,7 +9,7 @@ async def handle_message(client, message):
     if message.text.startswith("@admin"):
         report_text = message.text[6:]
         report_time = f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
-        track_id = f"#MB{message.chat.id}"
+        track_id = f"#MB{random.randint(1, 1000000)}"
         report = f"Reporter: {message.from_user.username}\nReporter Id: {message.from_user.id}\nTrack id: {track_id}\nReport Text: {report_text}\nReport Time: {report_time}"
         await message.reply(report)
 
