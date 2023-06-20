1bfb81e616ff6728da17871dee9b88d7ac911a00
Vikramved <121682766+Vikramved@users.noreply.github.com>
2023-06-20 21:37:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,19 @@
-from pyrogram import Client, filters
-import time
+import pyrogram
 
-app = Client("my_bot", 
-            api_id="15428219", 
-            api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", 
-            bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc"
-            )
+bot = pyrogram.Client("my_bot", api_id="15428219", api_hash="0042e5b26181a1e95ca40a7f7c51eaa7", bot_token="5166769555:AAFM8gtzAOJ4H9MRteci8QSvjO4f6m8YTCc")
 
-# Define the filter for messages containing "@admin"
-@admin_filter := filters.create(lambda client, _, message: "@admin" in message.text.lower())
+@bot.on_message()
+async def handle_message(message):
+    if message.text.startswith("@admin"):
+        report_text = message.text[6:]
+        report_time = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+        track_id = f"#MB{message.chat.id}"
+        await message.reply("Report sent successfully")
+        await message.reply(f"Reporter: {message.from_user.username}")
+        await message.reply(f"Reporter Id: {message.from_user.id}")
+        await message.reply(f"Track id: {track_id}")
+        await message.reply(f"Report Text: {report_text}")
+        await message.reply(f"Report Time: {report_time}")
 
-# Handle the messages that match the filter
-@app.on_message(admin_filter)
-def handle_admin_message(client, message):
-    # Get the sender's username and Telegram user id
-    sender_username = message.from_user.username
-    sender_id = message.from_user.id
-
-    # Get the report text and current time
-    report_text = message.text.replace("@admin", "").strip()
-    report_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-
-    # Build the reply message
-    reply_message = (
-        "Report sent successfully\n"
-        f"Reporter: {sender_username}\n"
-        f"Reporter Id: {sender_id}\n"
-        f"Track id: #MB{sender_id % 10}{(sender_id % 10) + 1}\n"
-        f"Report Text: {report_text}\n"
-        f"Report Time: {report_time}"
-    )
-
-    # Reply to the message
-    message.reply_text(reply_message)
-
-
-# Start the bot
-print("💝💝💝💝💝💝💝💝")
-app.run()
+print("❗️🙌🏻❗️🙌🏻❗️")
+bot.run()
